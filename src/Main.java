6c1285567c01498e26da49e77c95ec3545ebe11a
Vladlen <vladlen@vozhzhaev.ru>
2021-10-25 14:31:06 +0300

    Проверка ip адресов в однопоточном режиме

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Main {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("C:/java/ip.txt");
+            int i;
+            String resultIp = "";
+            while ((i = fis.read()) != -1){
+                if(i == 13) continue; // Символ возврата каретки
+                else if(i == 10){ // Символ переноса строки
+                    String[] resultArray = resultIp.split(":");
+                    String ip = resultArray[0];
+                    int port = Integer.parseInt(resultArray[1]);
+                    checkProxy(ip, port);
+                    resultIp = "";
+                }else if(i == 9){
+                    resultIp += ":";
+                } else{
+                    resultIp += (char) i;
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void checkProxy(String ip, int port){
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP,new InetSocketAddress(ip, port));
+            URL url =  new URL("https://vozhzhaev.ru/test.php");
+            URLConnection connection = url.openConnection(proxy);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            connection.getInputStream()));
+
+            System.out.println("ip: "+ip+":"+port+" рабочий");
+
+            in.close();
+        } catch (Exception e) {
+            System.out.println("ip: "+ip+":"+port+" НЕ РАБОТАЕТ");
+        }
+    }
 }
